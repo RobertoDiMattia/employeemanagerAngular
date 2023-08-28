48939f9bcc194aca006328527d37ac1c9fca7bc2
robertogatsu <robertogatsu@libero.it>
2023-08-28 15:24:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService} from './employeeService';
+import { Employee } from './employee';
+import { HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'employeemanagerapp';
+  
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+      this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+      );
+  }
 }
